014b4e18b965ce3b725db693a37e10984c05cceb
Juzdzewski Matthieu <matthieu.juzdzewski@etu.univ-nantes.fr>
2020-04-12 20:16:05 +0200

    Upload New File

---
+++ sources/Generator XML to Java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		Generator gen = new Generator("C:\\Users\\Matt\\Desktop\\FACM2Data\\TER\\test2.xml", "C:\\Users\\Matt\\Desktop\\FACM2Data\\TER");
+		gen.StartGenerationFromXML();
+
+	}
+
+}
