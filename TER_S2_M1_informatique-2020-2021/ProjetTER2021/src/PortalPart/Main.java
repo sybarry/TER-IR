5822b62bb8d6e84ccf276fa1343c643ed522ef85
anthony-44 <anthony.rozen@gmail.com>
2021-02-26 15:09:00 +0100

    ajout des sites consultés

--- TER_S2_M1_informatique-2020-2021/ProjetTER2021/src/PortalPart/Main.java
+++ TER_S2_M1_informatique-2020-2021/ProjetTER2021/src/PortalPart/Main.java
@@ -3,6 +3,17 @@
  */
 package PortalPart;
 
+import java.util.Collection;
+
+import javax.jws.WebParam.Mode;
+
+import lejos.hardware.BrickFinder;
+import lejos.hardware.LocalBTDevice;
+import lejos.hardware.RemoteBTDevice;
+import lejos.hardware.ev3.EV3;
+import lejos.remote.nxt.BTConnector;
+import lejos.remote.nxt.NXTConnection;
+
 /**
  * @author TER2021 : Gicquel, Guérin, Rozen
  *
@@ -34,8 +45,7 @@ public class Main {
 				
 			}
 			
-		}
-		
+		}		
 	}
 
 }
