e5a3440bcabcf4b09222de4012593d3a93c69020
Alexandre GICQUEL <alexandre.gicquel@etu.univ-nantes.fr>
2021-11-22 15:55:31 +0100

    upgrade code vehicle

--- Capstone2021-2022/code-Java/Capstone-Vehicle/src/VehicleApp/VehicleController.java
+++ Capstone2021-2022/code-Java/Capstone-Vehicle/src/VehicleApp/VehicleController.java
@@ -5,47 +5,27 @@ import Components.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 import lejos.hardware.Bluetooth;
-import lejos.hardware.BrickFinder;
 import lejos.hardware.port.MotorPort;
 import lejos.remote.nxt.BTConnection;
 import lejos.remote.nxt.BTConnector;
 import lejos.remote.nxt.NXTConnection;
 import lejos.robotics.RegulatedMotor;
-import lejos.hardware.Button;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.Sound;
-import lejos.hardware.ev3.EV3;
 
 public class VehicleController extends Thread {
 
 	private static BTConnection BTLink;
 	private static boolean appliReady;
 	private static int transmission = 0;
-	// private Battery battery;
-	// Valeur max 8400 min 6300
 
-	private static final int port = 1234;
-	private static EV3 brick;
-	private static ServerSocket server;
-	private static Socket client;
-	private static OutputStream out;
-	private static DataOutputStream dOut;
-	private static String nameLocal = "";
 
 	private static Motor MotorRight;
 	private static Motor MotorLeft;
-	private static MotionDetector motionSensor;
-	private static ContactSensor contactSensor;
 
 	private static int speed = 10;
 	
 	final static BTListener EBT = new BTListener();
-	private static final int MODE_AUTOMATIC = 19;
 
 	/*
 	 * Main class - launch the application
@@ -61,18 +41,13 @@ public class VehicleController extends Thread {
 
 		System.out.println("Connection BT success");
 
-		brick = (EV3) BrickFinder.getLocal();
-		nameLocal = brick.getName();
 
 		// Signals the application to be ready
 		ready(true);
 
 		// Initialisation of the application components
-		MotorRight = new Motor(MotorPort.C);
+		MotorRight = new Motor(MotorPort.A);
 		MotorLeft = new Motor(MotorPort.B);
-		motionSensor = new MotionDetector(SensorPort.S1);
-		contactSensor = new ContactSensor(SensorPort.S2);
-		// colorSensor = new ColorSensor(SensorPort.S3);
 
 		RegulatedMotor listMotors[] = { MotorRight.getOneMotor() };
 		MotorLeft.getOneMotor().synchronizeWith(listMotors);
@@ -80,25 +55,6 @@ public class VehicleController extends Thread {
 		// Stops the different Motors due to security issues
 		MotorRight.stop();
 		MotorLeft.stop();
-
-		new Thread() {
-			public void run() {
-				try {
-					wifiConnection();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				for (;;) {
-					try {
-						Button.DOWN.waitForPressAndRelease();
-						dOut.writeUTF(nameLocal);
-						dOut.flush();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}.start();
 
 
 		// Loop running as long as the application is running
@@ -116,57 +72,12 @@ public class VehicleController extends Thread {
 				secureBackward();
 				break;
 			case 3:
-				stopMotors();
-				break;
-			case 4:
-				changeSPEED(0);
-				break;
-			case 5:
-				changeSPEED(1);
-				break;
-			case 6:
-				changeSPEED(2);
-				break;
-			case 7:
-				changeSPEED(3);
-				break;
-			case 8:
-				changeSPEED(4);
-				break;
-			case 9:
-				changeSPEED(5);
-				break;
-			case 10:
-				changeSPEED(6);
-				break;
-			case 11:
-				changeSPEED(7);
-				break;
-			case 12:
-				changeSPEED(8);
-				break;
-			case 13:
-				changeSPEED(9);
-				break;
-			case 14:
-				changeSPEED(10);
-				break;
-			case 15:
-				ready(false);
-				break;
-			case 16:
 				turnRight();
 				break;
-			case 17:
+			case 4:
 				turnLeft();
 				break;
-			case 18:
-				honks();
-				break;
-			case 19:
-				modeAuto();
-				break;
-			case 20:
+			case 5:
 				stopMotors();
 				break;
 			default:
@@ -193,30 +104,6 @@ public class VehicleController extends Thread {
 		}
 	}
 
-	public static void wifiConnection() throws IOException {
-		server = new ServerSocket(port);
-		System.out.println("Awaiting client..");
-		client = server.accept();
-		System.out.println("CONNECTED");
-		out = client.getOutputStream();
-		dOut = new DataOutputStream(out);
-	}
-
-	public static void modeAuto() throws InterruptedException {
-		while (transmission == MODE_AUTOMATIC) {
-			if (!motionSensor.obstacleDetect()) {
-				// SPEEDLumininosite();
-				forward();
-			} else if (motionSensor.obstacleDetect()) {
-				turnRight();
-				try {
-					Thread.sleep(300);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
 
 	/*
 	 * Allows the car to move forward
@@ -233,11 +120,7 @@ public class VehicleController extends Thread {
 	}
 
 	public static void secureBackward() throws InterruptedException {
-		if (!contactSensor.contactDetected()) {
 			backWard();
-		} else {
-			stopMotors();
-		}
 	}
 
 	/*
@@ -261,14 +144,6 @@ public class VehicleController extends Thread {
 		MotorRight.stop();
 		MotorLeft.stop();
 		MotorLeft.getOneMotor().endSynchronization();
-	}
-
-	/*
-	 * Change the car's SPEED
-	 */
-	public static void changeSPEED(int newSPEED) {
-		speed = newSPEED;
-		System.out.println("SPEED : " + speed);
 	}
 
 	/*
@@ -301,14 +176,6 @@ public class VehicleController extends Thread {
 		MotorRight.speedUp(1);
 		MotorLeft.speedUp(1);
 		// TimeUnit.SECONDS.sleep(1);
-	}
-
-	/*
-	 * Allows the car to honk
-	 */
-	public static void honks() {
-		System.out.println("HONKS");
-		Sound.buzz();
 	}
 
 }
